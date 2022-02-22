7c669731bfbf5f45245f695802e88f94356f0804
Geekers-Joel237 <ivanjoelb6@gmail.com>
2022-02-22 15:08:59 +0100

    draw clock hands

--- graphics/canvasClock/dist/main.js
+++ graphics/canvasClock/dist/main.js
@@ -1,6 +1,7 @@
 //part1 : create the canvas
 //part2: draw a clock face
 //part3: draw clock numbers
+//part4: draw clock hands
 var canvas = document.getElementById('myCanvas');
 var ctx = canvas.getContext("2d");
 var radius = canvas.height / 2;
@@ -11,6 +12,7 @@ drawClock();
 function drawClock() {
     drawFace(ctx,radius);
     drawNumbers(ctx,radius);
+    drawTime(ctx, radius);
 }
 
 function drawFace(ctx,radius) {
@@ -52,3 +54,33 @@ function drawNumbers(ctx,radius) {
     }
     
 }
+
+function drawTime(ctx,radius) {
+    var now = new Date();
+    var hour = now.getHours();
+    var minute = now.getMinutes();
+    var second = now.getSeconds();
+
+    //hour
+    hour %= 12;
+    hour = (hour * Math.PI / 6) + (minute * Math.PI / (6 * 60)) + 
+            (second * Math.PI / (360*60));
+    drawHand(ctx, hour, radius*0.5, radius*0.07);
+    //minute
+    minute = (minute*Math.PI/30)+(second*Math.PI/(30*60));
+    drawHand(ctx, minute, radius*0.8, radius*0.07);
+    //second
+    second = (second*Math.PI/30);
+    drawHand(ctx, second, radius*0.9, radius*0.02);
+}
+
+function drawHand(ctx, pos, length, width) {
+    ctx.beginPath();
+    ctx.lineWidth = width;
+    ctx.lineCap = "round";
+    ctx.moveTo(0,0);
+    ctx.rotate(pos);
+    ctx.lineTo(0, -length);
+    ctx.stroke();
+    ctx.rotate(-pos);
+  }
